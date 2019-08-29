10c6ef13d391c2bbcc66e97dd3cb9d5ec608437b
Tommy Park <tjpark@wustl.edu>
2019-08-29 17:23:55 -0500

    Studio1

---
+++ src/studio1/Average.java
@@ -0,0 +1,14 @@
+package studio1;
+import support.cse131.ArgsProcessor;
+public class Average {
+
+    public static void main(String[] args){
+        ArgsProcessor ap = new ArgsProcessor(args);
+        int n1 = ap.nextInt("Value for n1?");
+        int n2 = ap.nextInt("Value for n2?");
+        double x = (n1 + n2)/2.0;
+        System.out.print(x);
+
+
+    }
+}
